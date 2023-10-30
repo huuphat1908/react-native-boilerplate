dda39605658965db11ea0a844d653438be26abc1
Nguyen Huu Phat <huuphat1908@gmail.com>
2023-10-30 23:43:15 +0700

    absolute import

---
+++ src/components/ui/SampleUIComp/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const SampleUIComp = () => {
+  return (
+    <View>
+      <Text>Sample UI Component</Text>
+    </View>
+  );
+};
+
+export default SampleUIComp;
