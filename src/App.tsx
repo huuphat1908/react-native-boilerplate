5f5c41aaaab2640293433d81708a882b665d7764
Nguyen Huu Phat <huuphat1908@gmail.com>
2023-11-01 11:38:09 +0700

    react navigation installation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 import SampleUIComp from '@/components/ui/SampleUIComp'
+import { NavigationContainer } from '@react-navigation/native'
 
 function App(): JSX.Element {
-  return <SampleUIComp />
+  return (
+    <NavigationContainer>
+      <SampleUIComp />
+    </NavigationContainer>
+  )
 }
 
 export default App
--- src/components/ui/SampleUIComp/index.tsx
+++ src/components/ui/SampleUIComp/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native'
 const SampleUIComp = () => {
   return (
     <View>
-      <Text>Sample UI Component</Text>
+      <Text>Sample UI</Text>
     </View>
   )
 }
