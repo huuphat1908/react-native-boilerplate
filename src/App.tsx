a6fa6713fa558c7df7f1a6c8cfe7727b0608a9bc
Nguyen Huu Phat <huuphat1908@gmail.com>
2023-11-15 06:37:44 +0000

    drawer installation

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
+import 'react-native-gesture-handler'
+
 import React from 'react'
 import { Text } from 'react-native'
-import Config from 'react-native-config'
 import { UnistylesTheme } from 'react-native-unistyles'
 import { QueryClientProvider } from 'react-query'
 
 import { reactQueryClient } from '@/libs'
 import { theme } from '@/libs/styleManager'
+import { createDrawerNavigator } from '@react-navigation/drawer'
 import { NavigationContainer } from '@react-navigation/native'
+
+const Drawer = createDrawerNavigator()
+
+const Feed = () => <Text>Feed</Text>
+const Article = () => <Text>Article</Text>
 
 function App(): JSX.Element {
   return (
     <QueryClientProvider client={reactQueryClient}>
       <UnistylesTheme theme={theme}>
         <NavigationContainer>
-          <Text>{Config.ENV}</Text>
+          <Drawer.Navigator>
+            <Drawer.Screen name="Feed" component={Feed} />
+            <Drawer.Screen name="Article" component={Article} />
+          </Drawer.Navigator>
         </NavigationContainer>
       </UnistylesTheme>
     </QueryClientProvider>
