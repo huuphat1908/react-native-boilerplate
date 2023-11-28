c7eb1be38c9330a6248798fb09ce4ddaaea988c3
Nguyen Huu Phat <huuphat1908@gmail.com>
2023-11-28 06:32:07 +0000

    add layouts folder to components folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,6 +8,13 @@ export { default as Switch } from './form/Switch'
 export { default as TextArea } from './form/TextArea'
 export { default as TimeInput } from './form/TimeInput'
 
+//layout
+export { default as Box } from './layout/Box'
+export { default as Center } from './layout/Center'
+export { default as HStack } from './layout/HStack'
+export { default as ScrollView } from './layout/ScrollView'
+export { default as VStack } from './layout/VStack'
+
 //typography
 export { default as Body } from './typography/Body'
 export { default as H1 } from './typography/H1'
@@ -21,13 +28,8 @@ export { default as Link } from './typography/Link'
 
 //ui
 export { default as Accordion } from './ui/Accordion'
-export { default as Box } from './ui/Box'
-export { default as Center } from './ui/Center'
 export { default as Divider } from './ui/Divider'
 export { default as ErrorText } from './ui/ErrorText'
-export { default as HStack } from './ui/HStack'
 export { default as Icon } from './ui/Icon'
 export { default as PrimaryButton } from './ui/PrimaryButton'
-export { default as ScrollView } from './ui/ScrollView'
 export { default as SecondaryButton } from './ui/SecondaryButton'
-export { default as VStack } from './ui/VStack'
