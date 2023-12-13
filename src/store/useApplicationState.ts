d2749be141d37af0d80e87420aa6556de854bef0
huuphat1908 <62057004+huuphat1908@users.noreply.github.com>
2023-12-13 16:15:47 +0000

    Client state and persist state (#16)
    * define application state
    
    * inject datetime format from store to datetime input
    
    * add immer, type for actions to application store
    
    * split to initialState and remove immer from application state
    
    * add zustand persist storage

---
+++ src/store/useApplicationState.ts
@@ -0,0 +1,56 @@
+import { create } from 'zustand'
+import { createJSONStorage, persist } from 'zustand/middleware'
+
+import { zustandPersistStorage } from '@/libs'
+import { LanguageCode } from '@/types/i18next'
+
+type AppTheme = 'Light' | 'Dark' | 'Setting'
+
+type DateFormat = 'DD/MM/YYYY' | 'MM/DD/YYYY'
+
+type TimeFormat = 'HH:mm'
+
+type State = {
+  theme: AppTheme
+  language: LanguageCode
+  dateFormat: DateFormat
+  timeFormat: TimeFormat
+  isLoggedIn: boolean
+}
+
+type Actions = {
+  setTheme: (theme: AppTheme) => void
+  setLanguage: (language: LanguageCode) => void
+  setDateFormat: (dateFormat: DateFormat) => void
+  setTimeFormat: (timeFormat: TimeFormat) => void
+  login: () => void
+  logout: () => void
+}
+
+const initialState: State = {
+  theme: 'Light',
+  language: 'en',
+  dateFormat: 'DD/MM/YYYY',
+  timeFormat: 'HH:mm',
+  isLoggedIn: false,
+}
+
+const useApplicationState = create<State & Actions>()(
+  persist(
+    set => ({
+      ...initialState,
+      setTheme: theme => set(() => ({ theme })),
+      setLanguage: language => set(() => ({ language })),
+      setDateFormat: dateFormat => set(() => ({ dateFormat })),
+      setTimeFormat: timeFormat => set(() => ({ timeFormat })),
+      login: () => set(() => ({ isLoggedIn: true })),
+      logout: () => set(() => ({ isLoggedIn: false })),
+    }),
+    {
+      name: 'application-storage',
+      storage: createJSONStorage(() => zustandPersistStorage),
+    },
+  ),
+)
+
+export default useApplicationState
