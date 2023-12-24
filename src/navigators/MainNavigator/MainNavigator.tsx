baa52c2449a9e1ec2322a04f42861becd9f9323f
huuphat1908 <huuphat1908@gmail.com>
2023-12-25 01:21:34 +0700

    change order of screen in main navigatior

--- src/navigators/MainNavigator/MainNavigator.tsx
+++ src/navigators/MainNavigator/MainNavigator.tsx
@@ -21,21 +21,21 @@ import Header from './Header'
 const Drawer = createDrawerNavigator<MainParamList>()
 
 export enum MainRoutes {
+  COLOR = 'Color',
+  TYPOGRAPHY = 'Typography',
   FORM_ELEMENT = 'Form Element',
   BUTTON = 'Button',
-  COLOR = 'Color',
-  COMPONENT = 'Component',
   DIALOG = 'Dialog',
-  TYPOGRAPHY = 'Typography',
+  COMPONENT = 'Component',
 }
 
 export type MainParamList = {
+  [MainRoutes.COLOR]: undefined
+  [MainRoutes.TYPOGRAPHY]: undefined
   [MainRoutes.FORM_ELEMENT]: undefined
   [MainRoutes.BUTTON]: undefined
-  [MainRoutes.COLOR]: undefined
-  [MainRoutes.COMPONENT]: undefined
   [MainRoutes.DIALOG]: undefined
-  [MainRoutes.TYPOGRAPHY]: undefined
+  [MainRoutes.COMPONENT]: undefined
 }
 
 const MainNavigator = () => {
@@ -51,18 +51,18 @@ const MainNavigator = () => {
       screenOptions={{
         header: renderHeader,
       }}>
+      <Drawer.Screen name={MainRoutes.COLOR} component={ColorScreen} />
+      <Drawer.Screen
+        name={MainRoutes.TYPOGRAPHY}
+        component={TypographyScreen}
+      />
       <Drawer.Screen
         name={MainRoutes.FORM_ELEMENT}
         component={FormElementScreen}
       />
       <Drawer.Screen name={MainRoutes.BUTTON} component={ButtonScreen} />
-      <Drawer.Screen name={MainRoutes.COLOR} component={ColorScreen} />
-      <Drawer.Screen name={MainRoutes.COMPONENT} component={ComponentScreen} />
       <Drawer.Screen name={MainRoutes.DIALOG} component={DialogScreen} />
-      <Drawer.Screen
-        name={MainRoutes.TYPOGRAPHY}
-        component={TypographyScreen}
-      />
+      <Drawer.Screen name={MainRoutes.COMPONENT} component={ComponentScreen} />
     </Drawer.Navigator>
   )
 }
