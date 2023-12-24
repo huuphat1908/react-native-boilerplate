84d53e49930ae525107ed5844a4f59638093854f
huuphat1908 <62057004+huuphat1908@users.noreply.github.com>
2023-12-25 00:16:22 +0700

    Splash screen (#25)
    * splash screen ios
    
    * splash screen android
    
    * remove unused storyboard in ios
    
    * android duplicate res folder to flavors
    
    * android - sync status bar background color with header

--- src/navigators/AppNavigator/index.tsx
+++ src/navigators/AppNavigator/index.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { hide } from 'react-native-bootsplash'
 
 import { NavigationContainer } from '@react-navigation/native'
 
 import MainNavigator from '../MainNavigator'
 
 const AppNavigator = () => {
+  const hideSplashScreen = useCallback(async () => {
+    await hide({
+      fade: true,
+    })
+  }, [])
+
   return (
-    <NavigationContainer>
+    <NavigationContainer onReady={hideSplashScreen}>
       <MainNavigator />
     </NavigationContainer>
   )
